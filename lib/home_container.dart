3ca123a3d606fa45effd52808b9794e8c777df17
L <554966574@qq.com>
2018-10-09 16:41:55 +0800

    实现底部tab

--- lib/home_container.dart
+++ lib/home_container.dart
@@ -13,17 +13,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var tabImages;
+  int _tabIndex = 0;
+  var appBarTitles = ['home', 'find', 'setting'];
+
   @override
   Widget build(BuildContext context) {
+    initData();
     return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(items: [
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home), title: Text('Home')),
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.book), title: Text('Book')),
-          BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.settings), title: Text('Setting')),
-        ]),
+        tabBar: CupertinoTabBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: getTabIcon(0),
+                title: getTabTitle(0)),
+            BottomNavigationBarItem(
+                icon: getTabIcon(1),
+                title: getTabTitle(1)),
+            BottomNavigationBarItem(
+                icon: getTabIcon(2),
+                title: getTabTitle(2)),
+          ],
+          onTap: (index) {
+            setState(() {
+              _tabIndex = index;
+            });
+          },
+        ),
         tabBuilder: (BuildContext context, int index) {
           return CupertinoTabView(
             builder: (BuildContext context) {
@@ -46,4 +61,49 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
     }
   }
-}
+
+  //根据选择获得对应的normal或是press的icon
+  Image getTabIcon(int curIndex) {
+    if (curIndex == _tabIndex) {
+      return tabImages[curIndex][1];
+    }
+    return tabImages[curIndex][0];
+  }
+
+  //获取bottomTab的颜色和文字
+  Text getTabTitle(int curIndex) {
+    if (curIndex == _tabIndex) {
+      return new Text(appBarTitles[curIndex],
+          style: new TextStyle(fontSize: 14.0, color: const Color(0xFF00D145)));
+    } else {
+      return new Text(appBarTitles[curIndex],
+          style: new TextStyle(fontSize: 14.0, color: const Color(0xff515151)));
+    }
+  }
+
+
+    void initData() {
+      //初始化选中和未选中的icon
+      tabImages = [
+        [
+          getTabImage('images/ic_nav_news_normal.png'),
+          getTabImage('images/ic_nav_news_actived.png')
+        ],
+        [
+          getTabImage('images/ic_nav_my_normal.png'),
+          getTabImage('images/ic_nav_my_pressed.png')
+        ],
+        [
+          getTabImage('images/ic_nav_discover_normal.png'),
+          getTabImage('images/ic_nav_discover_actived.png')
+        ]
+      ];
+    }
+
+    /*
+   * 根据image路径获取图片
+   */
+    Image getTabImage(path) {
+      return new Image.asset(path, width: 20.0, height: 20.0);
+    }
+  }
